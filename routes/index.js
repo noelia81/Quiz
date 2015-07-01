513d76415f598f89a37aa96db540616044bb3459
Noelia <nramirez@indra.es>
2015-07-01 11:31:27 +0200

    BBDD3

--- routes/index.js
+++ routes/index.js
@@ -18,6 +18,6 @@ router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 
 
-router.get('/author', quizController.author);
+//router.get('/author', quizController.author);
 
 module.exports = router;
