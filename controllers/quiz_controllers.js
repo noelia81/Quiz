b8f7d59fc9385a89d8f9f7c7fc7bad8c2fd6abc2
Noelia <nramirez@indra.es>
2015-07-03 11:38:38 +0200

    anadir

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -101,6 +101,35 @@ quiz // save: guarda en DB campos pregunta y respuesta de quiz
 }
 };
 
+exports.edit = function (req, res) {
+	var quiz = req.quiz;
+	res.render('quizes/edit', {quiz: quiz, errors: [] });
+};
+
+
+// PUT /quizes/:Id
+exports.update = function(req,res){
+		req.quiz.pregunta = req.body.quiz.pregunta;
+		req.quiz.respuesta = req.body.respuesta;
+		
+		req.quiz
+  .validate()
+  .then(
+    function(err){
+      if (err) {
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      } else {
+        req.quiz     // save: guarda campos pregunta y respuesta en DB
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes');});
+      }     // Redirección HTTP a lista de preguntas (URL relativo)
+    }
+  ).catch(function(error){next(error)});
+};
+
+
+
+
 
 //Exports.author = function (req, res){
 
