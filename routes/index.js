93d27bfe25c36f11728fe55cb77d01034f18fbfd
Noelia <nramirez@indra.es>
2015-07-15 12:27:44 +0200

    login

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var quizController = require('../controllers/quiz_controllers');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -12,12 +13,16 @@ router.get('/', function(req, res) {
 
 router.param('quizId', quizController.load); //autoload  :quizId
 
+//Definicion de rutas de session
+router.get('/login',  sessionController.new);//formulario login
+router.post('/login', sessionController.create);//crear sesion
+router.get('/logout', sessionController.destroy);//destruir sesion
 
 //Antes de la BBDD
 //router.get('/quizes/question', quizController.question);
 //router.get('/quizes/answer', quizController.answer);
 
-//con BBDD
+//con BBDD, Definicion de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
@@ -26,6 +31,8 @@ router.post('/quizes/create', quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizes/:quizId(\\d+)', quizController.update);
 router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+
+//Definicion de rutas de comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
 
