03b8fbface1621181a253aaee2862f768cf71352
Noelia <nramirez@indra.es>
2015-07-15 13:05:13 +0200

    login

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -8,6 +8,6 @@ exports.autenticar = function(login, password, callback) {
 	if(users[login]){
 		if(password === users[login].password){
 			callback(null, users[login]);
-		} else{ callback(new Error('password erróneo.'));}
+		} else{ callback(new Error('Password erróneo.'));}
 	}else{callback(new Error('No existe el usuario.'));}
 };
